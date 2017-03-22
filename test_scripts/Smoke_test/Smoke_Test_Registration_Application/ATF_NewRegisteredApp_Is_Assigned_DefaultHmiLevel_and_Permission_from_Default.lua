99b82277b6e93a681809b977650014933d91dca7
Truong <TTKHanh@luxoft.com>
2017-03-22 16:12:18 +0700

    update OnPermissionsChange checking

--- test_scripts/Smoke_test/Smoke_Test_Registration_Application/ATF_NewRegisteredApp_Is_Assigned_DefaultHmiLevel_and_Permission_from_Default.lua
+++ test_scripts/Smoke_test/Smoke_Test_Registration_Application/ATF_NewRegisteredApp_Is_Assigned_DefaultHmiLevel_and_Permission_from_Default.lua
@@ -1,14 +1,17 @@
 ---------------------------------------------------------------------------------------------
 -- Requirement summary:
--- [APPLINK-19072]: [PolicyTableUpdate] OnStatusUpdate(UPDATE_NEEDED) on new PTU request
+-- APPLINK-22733]: [Policies] "default" policies assigned to the application and "priority" value
+--[APPLINK-19072]: [PolicyTableUpdate] OnStatusUpdate(UPDATE_NEEDED) on new PTU request
 
--- Description: PoliciesManager must notify HMI via SDL.OnStatusUpdate(UPDATE_NEEDED)
--- on any PTU trigger. Ford-specific and EXTERNAL_PROPRIETARY exception:
--- No notification should be sent on user requested PTU from HMI (via SDL.UpdateSDL request).
+-- Description: PoliciesManager must:
+-- Adds new appIDs to local policy table;
+-- Send OnHMIStatus with default hmi level to mobile
+-- Send OnPermissionsChange with RPCs from default section to mobile
+-- Start PTU process
 
 -- Precondition:
--- 1. App is configured to do not reply for OnSystemRequest.
--- 2. Device is consented
+-- 1. Device is consented
+-- 2. Policy is up-to-date
 
 -- Steps:
 -- 1. Register new App
@@ -22,6 +25,7 @@
 -----------------------------Required Shared Libraries---------------------------------------
 require('user_modules/all_common_modules')
 local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+local testCasesForPolicyTableSnapshot = require('user_modules/shared_testcases_genivi/testCasesForPolicyTableSnapshot')
 
 ------------------------------ Variables and Common Functions -------------------------------
 app = common_functions:CreateRegisterAppParameters(
@@ -31,6 +35,25 @@ preloaded_file = config.pathToSDL .. "sdl_preloaded_pt.json"
 copied_preload_file = config.pathToSDL .. "update_sdl_preloaded_pt.json"
 parent_item = {"policy_table","app_policies","default","default_hmi"}
 default_hmi = common_functions:GetParameterValueInJsonFile(preloaded_file, parent_item)
+
+local RPC_Base4 = {}
+local function Get_RPCs()
+  testCasesForPolicyTableSnapshot:extract_preloaded_pt()
+  for i = 1, #testCasesForPolicyTableSnapshot.preloaded_elements do
+    if ( string.sub(testCasesForPolicyTableSnapshot.preloaded_elements[i].name,1,string.len("functional_groupings.Base-4.rpcs.")) == "functional_groupings.Base-4.rpcs." ) then
+      local str = string.match(testCasesForPolicyTableSnapshot.preloaded_elements[i].name, "functional_groupings%.Base%-4%.rpcs%.(%S+)%.%S+%.%S+")
+
+      if(#RPC_Base4 == 0) then
+        RPC_Base4[#RPC_Base4 + 1] = str
+      end
+
+      if(RPC_Base4[#RPC_Base4] ~= str) then
+        RPC_Base4[#RPC_Base4 + 1] = str
+      end
+    end
+  end
+end
+Get_RPCs()
 
 ------------------------------------ Precondition -------------------------------------------
 
@@ -64,7 +87,46 @@ function Test:Register_NewApp_And_Verify_SDL_Send_Correct_Params_to_HMI()
   EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "PROPRIETARY" })
   self.mobileSession1:ExpectResponse(cid, {success = true, resultCode = "SUCCESS"})
   self.mobileSession1:ExpectNotification("OnHMIStatus", {hmiLevel = default_hmi, audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"})
-  self.mobileSession1:ExpectNotification("OnPermissionsChange", arrayRegisterNewApp)
+  self.mobileSession1:ExpectNotification("OnPermissionsChange")
+  :Do(function(_,_data)
+      local is_perm_item_receved = {}
+      for i = 1, #RPC_Base4 do
+        is_perm_item_receved[i] = false
+      end
+
+      local is_perm_item_needed = {}
+      for i = 1, #_data.payload.permissionItem[1].rpcName do
+        is_perm_item_needed[i] = false
+      end
+      for i = 1, #_data.payload.permissionItem do
+        for j = 1, #RPC_Base4 do
+          if(_data.payload.permissionItem[i].rpcName == RPC_Base4[j]) then
+            is_perm_item_receved[j] = true
+            is_perm_item_needed[i] = true
+            break
+          end
+        end
+      end
+
+      for i = 1,#is_perm_item_needed do
+        if (is_perm_item_needed[i] == false) then
+          common_functions:PrintError("RPC: ".._data.payload.permissionItem[i].rpcName.." should not be sent")
+          is_test_fail = false
+        end
+      end
+
+      for i = 1,#is_perm_item_receved do
+        if (is_perm_item_receved[i] == false) then
+          common_functions:PrintError("RPC: "..RPC_Base4[i].." is not sent")
+          is_test_fail = false
+        end
+      end
+
+      if(is_test_fail == true) then
+        self:FailTestCase("Test is FAILED. See prints.")
+      end
+      common_functions:PrintTable(RPC_Base4)
+    end)
 end
 
 -------------------------------------------Postconditions-------------------------------------
