6a8f982d515c75922ba77a2d9ddd20cc8d7e67e4
Truong <TTKHanh@luxoft.com>
2017-05-17 10:24:08 +0700

    update according to review #1

--- test_scripts/Polices/Validation_of_PolicyTables/ATF_Policies_PTU_RequestType_array_is_empty.lua
+++ test_scripts/Polices/Validation_of_PolicyTables/ATF_Policies_PTU_RequestType_array_is_empty.lua
@@ -24,6 +24,7 @@ local app = common_functions:CreateRegisterAppParameters(
 )
 local expected_on_hmi_status = {hmiLevel = "BACKGROUND", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"}
 local strIvsu_cacheFolder = common_functions:GetValueFromIniFile("SystemFilesPath") .. "/"
+local snapshot_file = strIvsu_cacheFolder .. "sdl_snapshot.json"
 local request_type = {
   "HTTP",
   "FILE_RESUME",
@@ -32,8 +33,8 @@ local request_type = {
   "AUTH_ACK",
   "PROPRIETARY",
   "LAUNCH_APP",
-  -- "QUERY_APPS", -- SDL behaviour in case SDL 4.0 feature is required to be ommited in implementation (UNSUPPORTED_RESOURCE)
-  --"LOCK_SCREEN_ICON_URL", -> applicable to OnSystemRequest only
+  -- "QUERY_APPS", -> SDL behaviour in case SDL 4.0 feature is required to be omitted in implementation (UNSUPPORTED_RESOURCE)
+  -- "LOCK_SCREEN_ICON_URL", -> applicable to OnSystemRequest only
   "TRAFFIC_MESSAGE_CHANNEL",
   "DRIVER_PROFILE",
   "VOICE_SEARCH",
@@ -47,34 +48,40 @@ local request_type = {
   "FOTA"
 }
 
---[[ Local Functions ]]
 --[[ Specific Precondition ]]
 common_steps:AddNewTestCasesGroup("Preconditions")
-common_steps:PreconditionSteps("PreconditionSteps", const.precondition.ACTIVATE_APP)
-update_policy:updatePolicy("files/PTU_RequestType_empty.json", _, "PreconditionSteps_UpdatePolicy_With_RequestType_Of_App_SPT2_Is_Empty")
 
-function Test:Check_Request_Type_Is_Empty_In_SnapShot()
-  local file_name = strIvsu_cacheFolder .. "sdl_snapshot.json"
-  if common_steps:FileExisted(file_name) then
-    file_name = file_name
-  else
-    self:FailTestCase("snapshot is not exist")
-  end
-  local file = io.open(file_name, r)
-  local snapshot_file = file:read("*a")
-  local snapshot_data_table = json.decode(snapshot_file)
-
-  if not common_functions:CompareTables(snapshot_data_table.policy_table.app_policies.SPT2.RequestType,{}) then
-    self:FailTestCase("RequestType of " .. app.appID .. " is not empty")
+function Test:Remove_Existed_Snapshot_File()
+  if common_steps:FileExisted(snapshot_file) then
+    os.execute( "rm -f " .. snapshot_file)
   end
 end
 
+common_steps:PreconditionSteps("PreconditionSteps", const.precondition.ACTIVATE_APP)
+update_policy:updatePolicy("files/PTU_RequestType_empty.json", _, "PreconditionSteps_UpdatePolicy_With_RequestType_Of_App_SPT2_Is_Empty")
 common_steps:AddMobileSession("AddMobileSession", _, "mobileSession1")
 common_steps:RegisterApplication("Precondition_Register_App_" .. app.appID, "mobileSession1", app,_, expected_on_hmi_status)
 common_steps:ActivateApplication("Precondition_Activate_App_" .. app.appID, app.appName)
 
-for i=1, #request_type do
-  common_steps:AddNewTestCasesGroup("RequestType = " .. request_type[i] )
+function Test:Check_Request_Type_Is_Empty_In_SnapShot()
+  local count_sleep = 1
+  while not common_steps:FileExisted(snapshot_file) and count_sleep < 9 do
+    os.execute("sleep 1")
+    count_sleep = count_sleep + 1
+  end
+  if not common_steps:FileExisted(snapshot_file) then
+    self:FailTestCase("snapshot does not exist")
+    return
+  end
+  local snapshot_requestType = common_functions:GetParameterValueInJsonFile(snapshot_file, {"policy_table", "app_policies", "SPT2", "RequestType"})
+
+  if not common_functions:CompareTables(snapshot_requestType,{}) then
+    self:FailTestCase("RequestType of " .. app.appID .. " is not empty")
+  end
+end
+
+for i=1, #request_type -10 do
+  common_steps:AddNewTestCasesGroup("RequestType = " .. request_type[i])
 
   Test["Precondtion_PutFile_icon.png_" .. tostring(i)] = function(self)
     local cid = self.mobileSession1:SendRPC(
@@ -111,6 +118,4 @@ end
 
 --[[ Postconditions ]]
 common_steps:AddNewTestCasesGroup("Postconditions")
-function Test:PostconditionStopSDL()
-  StopSDL()
-end
+common_steps:StopSDL("Postcondition_StopSDL")
