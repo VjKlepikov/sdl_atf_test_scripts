7593dcf4464d3ad3bbfa90dc22fb35a1828cdd93
Truong <TTKHanh@luxoft.com>
2017-05-23 15:10:50 +0700

    update Check_SDL_continue_working function

--- test_scripts/Polices/Validation_of_PolicyTables/ATF_PreloadedPT_contains_one_invalid_other_valid_values_in_RequestType.lua
+++ test_scripts/Polices/Validation_of_PolicyTables/ATF_PreloadedPT_contains_one_invalid_other_valid_values_in_RequestType.lua
@@ -4,10 +4,10 @@
 
 -- Description:
 -- In case PreloadedPT has several values in "RequestType" array and one of them is invalid
--- SDL must:cut off this invalid value treat such PreloadedPT as valid continue working.
+-- sdl must:cut off this invalid value treat such PreloadedPT as valid continue working.
 
 -- Preconditions:
--- -- 1. SDL and HMI are started
+-- -- 1. sdl and HMI are started
 -- -- 2. Preloaded PT exists at the path defined in .ini file
 
 -- Steps:
@@ -15,8 +15,8 @@
 -- -- 2. PreloadedPT-> "app_policies" -> "default" -> RequestType has {TRAFFIC_MESSAGE_CHANNEL, HTTP, PROPRIETARY, IVSU}
 
 -- Expected result:
--- -- 1. SDL cuts off IVSU from RequestType
--- -- 2. SDL continue working
+-- -- 1. sdl cuts off IVSU from RequestType
+-- -- 2. sdl continue working
 
 --[[ Generic precondition ]]
 require('user_modules/all_common_modules')
@@ -49,7 +49,7 @@ function Test.PreconditionSteps_Update_RequestType_has_IVSU_In_PreloadedPT_file(
 end
 
 function Test.PreconditionSteps_Remove_Existing_Snapshot_File()
-  if common_steps:FileExisted(snapshot_file) then
+  if common_functions:IsFileExist(snapshot_file) then
     os.execute( "rm -f " .. snapshot_file)
   end
 end
@@ -58,33 +58,31 @@ common_steps:PreconditionSteps("PreconditionSteps", const.precondition.ACTIVATE_
 
 --[[ Test ]]
 common_steps:AddNewTestCasesGroup("Test")
-function Test:Check_SDL_cuts_off_IVSU_from_RequestType()
+function Test:Check_sdl_cuts_off_IVSU_from_RequestType()
   local count_sleep = 1
-  while not common_steps:FileExisted(snapshot_file) and count_sleep < 9 do
+  while not common_functions:IsFileExist(snapshot_file) and count_sleep < 9 do
     os.execute("sleep 1")
     count_sleep = count_sleep + 1
   end
 
-  if not common_steps:FileExisted(snapshot_file) then
+  if not common_functions:IsFileExist(snapshot_file) then
     self:FailTestCase("snapshot does not exist")
   end
-  local snapshot_requestType = common_functions:GetParameterValueInJsonFile(snapshot_file, parent_item)
+  local snapshot_request_type = common_functions:GetParameterValueInJsonFile(snapshot_file, parent_item)
 
-  if not common_functions:CompareTablesNotSorted(snapshot_requestType, request_type_after_cut_off) then
+  if not common_functions:CompareTablesNotSorted(snapshot_request_type, request_type_after_cut_off) then
     self:FailTestCase("RequestType in snapshot is incorrect. Please check!")
   end
 end
 
-function Test:Check_SDL_continue_working()
+function Test:Check_sdl_continue_working()
   os.execute(" sleep 5 ")
-  local status = sdl:CheckStatusSDL()
-  if (status == sdl.RUNNING) then
-    return true
+  if sdl:CheckStatusSDL() ~= sdl.RUNNING then
+    self:FailTestCase("sdl is stopped.")
   end
-  self:FailTestCase("SDL has already stopped.")
 end
 
 --[[ Postconditions ]]
 common_steps:AddNewTestCasesGroup("Postconditions")
 common_steps:RestoreIniFile("Postcondition_Restore_sdl_preloaded_pt.json", "sdl_preloaded_pt.json")
-common_steps:StopSDL("Postcondition_StopSDL")
+common_steps:StopSDL("Postcondition_Stopsdl")
